d6e73bf23f376ea3be98326f22dcfb728c92ac7e
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2024-12-13 23:00:28 +0900

    style(#5): 로고에 그림자 더하고 크기 작게 변경

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -5,10 +5,11 @@ interface StyledLogoProps {
 	size?: string;
 }
 
-const QRULogo = ({ size = '2rem' }: StyledLogoProps) => {
+const QRULogo = ({ size = '1.5rem' }: StyledLogoProps) => {
 	return (
 		<LogoWrapper>
 			<StyledLogo size={size} />
+			QRU
 		</LogoWrapper>
 	);
 };
@@ -17,16 +18,25 @@ const LogoWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	gap: 0.5rem;
+	font-size: 1.5rem;
+	font-weight: bold;
+	cursor: pointer;
+	color: ${({ theme }) => theme.color.text};
+	text-shadow: -2px 2px 2px rgba(0, 0, 0, 0.2);
+	overflow: visible;
+
+	&:hover {
+		transform: scale(1.1);
+		transition: transform 0.3s ease;
+	}
 `;
 
 const StyledLogo = styled(Logo)<StyledLogoProps>`
 	width: ${({ size }) => size};
 	height: ${({ size }) => size};
 	transition: transform 0.3s ease, fill 0.3s ease;
-
-	&:hover {
-		transform: scale(1.1); // 크기 확대 효과
-	}
+	overflow: visible;
 `;
 
 export default QRULogo;
