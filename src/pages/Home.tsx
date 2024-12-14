a68a0f2957caa0afd50eca7ffcbd811e6c4101cd
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2024-12-15 05:05:55 +0900

    refactor(#3): 버튼 컴포넌트를 children으로 전달하지 않고 QRULogoCard로 옮김

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,19 @@
-import React from 'react';
 import QRULogoCard from '../components/home/QRULogoCard';
-import Button from '../components/common/Button';
-import { FaPlus } from 'react-icons/fa';
 import styled from 'styled-components';
 
-const Home: React.FC = () => {
-	const handleCreateCard = () => {
-		alert('명함 만들기 버튼 클릭!');
-	};
-
+function Home() {
 	return (
 		<HomeStyle>
-			<div className="home-container">
-				<QRULogoCard>
-					<Button onClick={handleCreateCard} icon={<FaPlus />}>
-						나만의 명함 만들러 가기
-					</Button>
-				</QRULogoCard>
-			</div>
+			<QRULogoCard />
 		</HomeStyle>
 	);
-};
+}
 
 const HomeStyle = styled.div`
 	.home-container {
 		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		gap: 2rem;
 		flex-direction: column;
+		gap: 2rem;
 	}
 `;
 
