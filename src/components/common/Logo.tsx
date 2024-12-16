8ccb542716664f6ec46f783edbf459f9a9f185d8
KIMJEONGHYEON <68999618+CodingKirby@users.noreply.github.com>
2024-12-16 14:04:07 +0900

    refactor(#5): props 수정 & 구조 수정

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,21 +1,28 @@
 import styled from 'styled-components';
 import Logo from '../../assets/logo.svg?react';
+import { ColorKey, HeadingSize } from '../../styles/theme';
+import Title from './Title';
 
-interface StyledLogoProps {
-	size?: string;
-	color?: string;
+interface Props {
+	size: HeadingSize;
+	color?: ColorKey;
 }
 
-const QRULogo = ({ size = '1.5rem', color }: StyledLogoProps) => {
+const QRULogo = ({ size, color }: Props) => {
 	return (
-		<LogoWrapper>
-			<StyledLogo size={size} fill={color} />
-			QRU
-		</LogoWrapper>
+		<a href="/">
+			<LogoWrapper>
+				<StyledLogo size={size} fill={color} />
+				<Title size={size} color={color}>
+					QRU
+				</Title>
+			</LogoWrapper>
+		</a>
 	);
 };
 
 const LogoWrapper = styled.div`
+	height: 100%;
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -24,18 +31,17 @@ const LogoWrapper = styled.div`
 	font-weight: bold;
 	cursor: pointer;
 	color: ${({ theme }) => theme.color.text};
-	text-shadow: -2px 2px 2px rgba(0, 0, 0, 0.2);
+	text-shadow: -2px 2px 2px hsla(0, 0%, 0%, 0.2);
 	overflow: visible;
-
 	&:hover {
 		transform: scale(1.1);
 		transition: transform 0.3s ease;
 	}
 `;
 
-const StyledLogo = styled(Logo)<StyledLogoProps>`
-	width: ${({ size }) => size};
-	height: ${({ size }) => size};
+const StyledLogo = styled(Logo)<Omit<Props, 'children'>>`
+	width: ${({ theme, size }) => theme.heading[size].fontSize};
+	aspect-ratio: 1 / 1;
 	fill: ${({ theme }) => theme.color.primary};
 	transition: transform 0.3s ease, fill 0.3s ease;
 	overflow: visible;
