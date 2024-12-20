a1af3db963bb513133d841f14ed0d7b77be7a808
7evendays <codingkirby0@gmail.com>
2024-12-20 12:36:42 +0900

    style(#13): 스타일 수정

--- src/components/common/InputText.tsx
+++ src/components/common/InputText.tsx
@@ -1,24 +1,40 @@
-import React, { ForwardedRef } from 'react';
-import styled from 'styled-components';
+import React, { ForwardedRef } from "react";
+import styled from "styled-components";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-	placeholder?: string;
-	inputType?: 'text' | 'email' | 'password' | 'number';
+  placeholder?: string;
+  inputType?: "text" | "email" | "password" | "number";
 }
 
 const InputText = React.forwardRef(
-	({ placeholder, inputType, onChange, ...props }: Props, ref: ForwardedRef<HTMLInputElement>) => {
-		return <InputTextStyle placeholder={placeholder} ref={ref} type={inputType} onChange={onChange} {...props} />;
-	}
+  (
+    { placeholder, inputType, onChange, ...props }: Props,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <InputTextStyle
+        placeholder={placeholder}
+        ref={ref}
+        type={inputType}
+        onChange={onChange}
+        {...props}
+      />
+    );
+  }
 );
 
-const InputTextStyle = styled.input`
-	padding: 0.25rem 0.75rem;
-	border: 1px solid ${({ theme }) => theme.color.secondaryVariant};
-	border-radius: ${({ theme }) => theme.borderRadius.default};
-	font-size: 1rem;
-	line-height: 1.5;
-	color: ${({ theme }) => theme.color.text};
+const InputTextStyle = styled.input`a
+  outline: none;
+  border: none;
+  box-shadow: ${({ theme }) => theme.shadow.light};
+  overflow: visible;
+  padding: 0.25rem 0.75rem;
+  line-height: 1.8;
+
+  color: ${({ theme }) => theme.color.text};
+  background: ${({ theme }) => theme.color.secondary};
+  border-radius: ${({ theme }) => theme.borderRadius.default};
+  font-size: ${({ theme }) => theme.fontSize.extraSmall};
 `;
 
 export default InputText;
