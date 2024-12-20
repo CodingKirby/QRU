96cee3c89584cb9d0bbf1b9e237ff1f8fe604552
7evendays <codingkirby0@gmail.com>
2024-12-20 12:17:23 +0900

    style(#6): Header를 fixed로 바꾼 것에 맞춰서 레이아웃 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,36 +1,35 @@
-import styled from 'styled-components';
-import Header from '../common/Header';
-import Footer from '../common/Footer';
+import styled from "styled-components";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
 
 interface LayoutProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 function Layout({ children }: LayoutProps) {
-	return (
-		<LayoutStyle>
-			<Header />
-			<Content>{children}</Content>
-			<Footer />
-		</LayoutStyle>
-	);
+  return (
+    <LayoutStyle>
+      <Header />
+      <Content>{children}</Content>
+      <Footer />
+    </LayoutStyle>
+  );
 }
 
 const LayoutStyle = styled.div`
-	display: flex;
-	flex-direction: column;
-	min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 `;
 
 const Content = styled.main`
-	width: 100%;
-	margin: 0 auto;
-	max-width: ${({ theme }) => theme.layout.width.large};
-	padding: 20px 0;
-
-	@media screen AND ${({ theme }) => theme.mediaQuery.mobile} {
-		padding: 20px 12px;
-	}
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  padding: 4rem 2rem;
+  @media screen AND ${({ theme }) => theme.mediaQuery.mobile} {
+    padding: 4rem 1rem;
+  }
 `;
 
 export default Layout;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,28 @@
-import QRULogoCard from '../components/home/QRULogoCard';
-import styled from 'styled-components';
+import QRULogoCard from "../components/home/QRULogoCard";
+import styled from "styled-components";
 
 function Home() {
-	return (
-		<HomeStyle>
-			<section className="section">
-				<QRULogoCard />
-			</section>
-		</HomeStyle>
-	);
+  return (
+    <HomeStyle>
+      <section className="section">
+        <QRULogoCard />
+      </section>
+    </HomeStyle>
+  );
 }
 
 const HomeStyle = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 
-	.section {
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
+  .section {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export default Home;
