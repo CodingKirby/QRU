f40151ac08344f664edbcdf392ec5e1547990070
7evendays <codingkirby0@gmail.com>
2024-12-20 12:16:00 +0900

    fix(#21): 페이지 이동 시 흰화면 노출되는 것 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,72 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QRUThemeProvider } from './context/themeContext';
-import Layout from './components/layout/Layout';
-import Home from './pages/Home';
-import Auth from './pages/Auth';
-import MyPage from './pages/MyPage';
-import NewCard from './pages/NewCard';
-import About from './pages/About';
-import Shuffle from './pages/Shuffle';
-import NotFound from './pages/NotFound';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./api/queryClient";
+
+import { QRUThemeProvider } from "./context/themeContext";
+import Layout from "./components/layout/Layout";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
+import NewCard from "./pages/NewCard";
+import MyPage from "./pages/MyPage";
+import MyCard from "./pages/MyCard";
+import About from "./pages/About";
+import Shuffle from "./pages/Shuffle";
+import NotFound from "./pages/NotFound";
+import Error from "./components/common/Error";
+
+const routeList = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/mypage",
+    element: <MyPage />,
+  },
+  {
+    path: "/cards",
+    element: <NewCard />,
+  },
+  {
+    path: "/cards/:id",
+    element: <MyCard />,
+  },
+  {
+    path: "/cards/shuffle",
+    element: <Shuffle />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
+
+const router = createBrowserRouter(
+  routeList.map((item) => {
+    return {
+      ...item,
+      element: <Layout>{item.element}</Layout>,
+      errorElement: <Error />,
+    };
+  })
+);
 
 function App() {
-	return (
-		<Router>
-			<QRUThemeProvider>
-				<Layout>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/login" element={<Auth />} />
-						<Route path="/mypage" element={<MyPage />} />
-						<Route path="/card" element={<NewCard />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/shuffle" element={<Shuffle />} />
-
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</Layout>
-			</QRUThemeProvider>
-		</Router>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <QRUThemeProvider>
+        <RouterProvider router={router} />
+      </QRUThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
