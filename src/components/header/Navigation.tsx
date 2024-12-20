a1ddf9552257128139942309f5287c3c015c3a51
7evendays <codingkirby0@gmail.com>
2024-12-20 15:35:44 +0900

    fix(#6): 네비게이션 드롭다운 hover 오류(다른 item의 dropdown이 펼쳐지는 현상) 수정

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,17 +1,17 @@
-import Dropdown from "../common/Dropdown";
-import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 import { NAVIGATION } from "../../data/navigationItems";
+import Dropdown from "../common/Dropdown";
+import { styled } from "styled-components";
 
 function Navigation() {
   return (
-    <NavigationStyle className="navigation">
+    <NavigationStyle>
       <ul>
         {NAVIGATION.map((item, index) => (
           <li key={index}>
             <Link to={item.link}>{item.title}</Link>
             {item.subItems && (
-              <Dropdown>
+              <Dropdown className="navigation-dropdown">
                 {item.subItems.map((subItem, subIndex) => (
                   <Link to={subItem.link} key={subIndex} className="item">
                     {subItem.title}
@@ -44,84 +44,80 @@ const NavigationStyle = styled.nav`
 
         &:hover {
           color: ${({ theme }) => theme.color.primary};
-
-          > .panel {
-            visibility: visible;
-            transform: scaleY(1);
-            opacity: 1;
-            transition: transform 0.3s ease, opacity 0.3s ease;
-          }
-
-          > a {
-            color: ${({ theme }) => theme.color.primary};
-          }
-        }
-      }
-    }
-
-    .panel {
-      visibility: hidden;
-      display: flex;
-      flex-direction: column;
-
-      position: absolute;
-      top: 100%;
-      margin-top: 1rem;
-      padding: 1rem 1.2rem;
-      line-height: 2;
-
-      background: ${({ theme }) => theme.color.surface};
-      border-radius: ${({ theme }) => theme.borderRadius.default};
-      box-shadow: ${({ theme }) => theme.shadow.default};
-
-      transform-origin: top;
-      transform: scaleY(0);
-      opacity: 0;
-
-      .item {
-        font-size: ${({ theme }) => theme.fontSize.medium};
-        color: ${({ theme }) => theme.color.text};
-        text-decoration: none;
-        transition: color 0.3s ease;
-        border-radius: ${({ theme }) => theme.borderRadius.default};
-        padding: 0.3rem 1rem;
-
-        &:hover {
-          color: ${({ theme }) => theme.color.primary};
-          background: ${({ theme }) => theme.color.secondary};
-          box-shadow: ${({ theme }) => theme.shadow.default};
         }
       }
 
-      /* 화살표 */
-      &:before {
-        content: "";
-        position: absolute;
-        width: 0;
-        height: 0.5rem;
-        border-left: 0.5rem solid transparent;
-        border-right: 0.5rem solid transparent;
-        border-bottom: 0.5rem solid ${({ theme }) => theme.color.surface};
-        top: -0.5rem;
-        left: 2rem;
+      &:hover .navigation-dropdown {
+        visibility: visible;
+        opacity: 1;
+        transform: scaleY(1);
       }
 
-      &:after {
-        content: "";
+      .navigation-dropdown {
+        visibility: hidden;
         position: absolute;
-        top: -1rem;
+        top: calc(100% + 1rem);
         left: 0;
-        width: 100%;
-        height: 2rem;
-        background: none;
-      }
-    }
 
-    &:hover .panel {
-      visibility: visible;
-      transform: scaleY(1);
-      opacity: 1;
-      transition: transform 0.5s ease, opacity 0.3s ease;
+        opacity: 0;
+        transform-origin: top;
+        transform: scaleY(0);
+        display: flex;
+        flex-direction: column;
+        transition: all 0.3s ease;
+
+        .panel {
+          display: flex;
+          flex-direction: column;
+
+          position: relative;
+          top: 100%;
+          padding: 1rem 1.2rem;
+          line-height: 2;
+
+          background: ${({ theme }) => theme.color.surface};
+          border-radius: ${({ theme }) => theme.borderRadius.default};
+          box-shadow: ${({ theme }) => theme.shadow.default};
+
+          .item {
+            font-size: ${({ theme }) => theme.fontSize.medium};
+            color: ${({ theme }) => theme.color.text};
+            text-decoration: none;
+            transition: color 0.3s ease;
+            border-radius: ${({ theme }) => theme.borderRadius.default};
+            padding: 0.3rem 1rem;
+
+            &:hover {
+              color: ${({ theme }) => theme.color.primary};
+              background: ${({ theme }) => theme.color.secondary};
+              box-shadow: ${({ theme }) => theme.shadow.default};
+            }
+          }
+        }
+
+        /* 화살표 */
+        &:before {
+          content: "";
+          position: absolute;
+          width: 0;
+          height: 0.5rem;
+          border-left: 0.5rem solid transparent;
+          border-right: 0.5rem solid transparent;
+          border-bottom: 0.5rem solid ${({ theme }) => theme.color.surface};
+          top: -0.5rem;
+          left: 2rem;
+        }
+
+        &:after {
+          content: "";
+          position: absolute;
+          top: -1rem;
+          left: 0;
+          width: 100%;
+          height: 2rem;
+          background: none;
+        }
+      }
     }
   }
 
